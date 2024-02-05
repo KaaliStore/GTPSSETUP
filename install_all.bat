e426340fafe26d2487494ebaf60b00153e61bdef
KaaliStore <159053388+KaaliStore@users.noreply.github.com>
2024-02-05 23:55:48 +0700

    Add files via upload

---
+++ install_all.bat
@@ -0,0 +1,49 @@
+@echo off
+CD /d %~dp0
+
+echo.
+echo Auto Setup GTPS by Kaali
+echo t.me/kaalistore
+echo.
+echo Installing runtime packages...
+
+set IS_X64=0 && if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set IS_X64=1) else (if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (set IS_X64=1))
+
+if "%IS_X64%" == "1" goto X64
+
+echo Node Js...
+start /wait node-v20.11.0-x64.msi /qb
+
+echo Winrar...
+start /wait winrar-x64-624.exe /passive /norestart
+
+echo Notepad ++...
+start /wait npp.8.6.2.Installer.x64.exe /passive /norestart
+
+echo Visual Studio
+start /wait VisualStudioSetup.exe /passive /norestart
+
+goto END
+
+:X64
+
+echo Node Js...
+start /wait node-v20.11.0-x64.msi /passive /norestart
+
+echo Winrar...
+start /wait winrar-x64-624.exe /passive /norestart
+
+echo Notepad ++...
+start /wait npp.8.6.2.Installer.x64.exe /passive /norestart
+
+echo Visual Studio
+start /wait VisualStudioSetup.exe /passive /norestart
+
+goto END
+
+:END
+
+echo.
+echo Gtps Setup Already Installed
+
+exit
